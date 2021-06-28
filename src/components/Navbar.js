d1d8cb8c089a6ab6f38dc9d9ceab3e0a367af906
Cristina Plesa <cristinaplesa8@gmail.com>
2021-06-28 15:34:15 -0700

    add header and navbar components to subdirectory
    Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar(){
+  return (
+    <React.Fragment>
+      <h3>Places to Stay</h3>
+      <h3>Experiences</h3>
+      <h3>Online Experiences</h3>
+      <h3>Searchbar</h3>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Navbar;
